5f7012b59080298fe3079e06a2a456018f4656dc
mattecurro <matteo1curro@gmail.com>
2025-12-30 19:09:03 +0100

    Fix of arch (no bypassing of int in accumulation) for latency calculation in model.py.

--- levels.py
+++ levels.py
@@ -522,7 +522,7 @@ class MemLevel(Level):
 #                    print(f"DEBUG: layer_id={layer_id}, per_layer_int_in_reads={self.per_layer_int_in_reads[layer_id - 1]:,.0f}, per_layer_w_reads={self.per_layer_w_reads[layer_id]:,.0f}, last_out_writes={self.last_out_writes:,.0f}, out_reads={self.out_reads:,.0f}")
                 return self.per_layer_int_in_reads[layer_id - 1] + (self.out_reads - self.last_out_writes) + self.per_layer_w_reads[layer_id]
             else:
-                if (layer_id == 1 and self.name == "FeatureMemory"):
+                if (layer_id == 1):
                     print(f"DEBUG: layer_id={layer_id}, per_layer_int_in_reads={self.per_layer_int_in_reads[layer_id - 1]:,.0f}, per_layer_w_reads={self.per_layer_w_reads[layer_id]:,.0f}, per_layer_int_out_reads={self.per_layer_int_out_reads[layer_id]:,.0f}, last_per_layer_int_out_writes={self.last_per_layer_int_out_writes[layer_id]:,.0f}")
                 #return self.per_layer_int_in_reads[layer_id - 1] + self.per_layer_w_reads[layer_id] + (self.per_layer_int_out_reads[layer_id] - self.last_per_layer_int_out_writes[layer_id])
                 return self.per_layer_int_in_reads[layer_id - 1] + self.per_layer_w_reads[layer_id] + self.per_layer_int_out_reads[layer_id]
